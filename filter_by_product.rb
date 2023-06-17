67ca624fa65319b2beb4e460abced4d86f0b2314
Thomas Neves <tomasdtna@gmal.com>
2023-06-17 00:11:43 -0300

    refact: code refactoring

--- filter_by_product.rb
+++ filter_by_product.rb
@@ -4,54 +4,42 @@ products = []
 puts 'Product registration.'
 
 print "\nNumber of products to register (max #{PRODUCT_MAX}): "
-quantityOfProduct = nil
+quantity_of_product = gets.to_i
 
-loop do
-  quantityOfProduct = gets.chomp
-  if quantityOfProduct.empty?
-    print "Invalid input. Please enter a number between 1 and #{PRODUCT_MAX}: "
-    next
-  end
-
-  quantityOfProduct = quantityOfProduct.to_i
-  break if quantityOfProduct.between?(1, PRODUCT_MAX)
-
+while !(1..PRODUCT_MAX).include?(quantity_of_product)
   print "Invalid input. Please enter a number between 1 and #{PRODUCT_MAX}: "
+  quantity_of_product = gets.to_i
 end
 
-quantityOfProduct.times do |product|
+quantity_of_product.times do |product|
   code = nil
 
   loop do
     print "Enter the product code #{product + 1}: "
-    code = gets.chomp
+    code = gets.to_i
 
-    if code.empty?
+    if code.zero?
       puts 'Code cannot be empty. Please enter a valid code.'
-      next
+    elsif products.any? { |p| p[:code] == code }
+      puts 'This code has already been entered. Please enter a different code.'
+    else
+      break
     end
-
-    code = code.to_i
-    break unless products.any? { |product| product[:code] == code }
-
-    puts 'This code has already been entered. Please enter a different code.'
   end
 
   quantity = nil
 
   loop do
     print "Enter the quantity of the product #{product + 1}: "
-    quantity = gets.chomp
+    quantity = gets.to_i
 
-    if quantity.empty?
+    if quantity.zero?
       puts 'Quantity cannot be empty. Please enter a valid quantity.'
-      next
+    elsif quantity.negative?
+      puts 'Quantity must be a positive number.'
+    else
+      break
     end
-
-    quantity = quantity.to_i
-    break if quantity.positive?
-    
-    puts 'Quantity must be a positive number.'
   end
 
   products << { code: code, quantity: quantity }
@@ -61,22 +49,22 @@ puts "\nProduct query"
 
 loop do
   print "\nEnter the product code (or 0 to exit): "
-  queryProduct = gets.chomp
+  query_product = gets.chomp
 
-  break if queryProduct == '0'
+  break if query_product == '0'
 
-  if queryProduct.empty?
+  if query_product.empty?
     puts 'Code cannot be empty. Please enter a valid code.'
     next
   end
 
-  queryProduct = queryProduct.to_i
+  query_product = query_product.to_i
 
-  product = products.find { |example| example[:code] == queryProduct }
+  product = products.find { |p| p[:code] == query_product }
 
   if product
-    quantityFound = product[:quantity]
-    puts "Quantity found: #{quantityFound}"
+    quantity_found = product[:quantity]
+    puts "Quantity found: #{quantity_found}"
   else
     puts 'Product not found'
   end
