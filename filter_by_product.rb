eb15893f657d9556da747d12c678740284a6b891
Thomas Neves <tomasdtna@gmal.com>
2023-06-17 00:17:52 -0300

    feat: add translation

--- filter_by_product.rb
+++ filter_by_product.rb
@@ -69,3 +69,76 @@ loop do
     puts 'Product not found'
   end
 end
+
+#-------------------------------------------------- Translation into portuguese
+# PRODUCT_MAX = 100
+# products = []
+
+# ## Cadastro de produto.
+
+# Quantidade de produtos a serem cadastrados (máx #{PRODUCT_MAX}): 
+# quantity_of_product = gets.to_i
+
+# while !(1..PRODUCT_MAX).include?(quantity_of_product)
+#   print "Entrada inválida. Por favor, insira um número entre 1 e #{PRODUCT_MAX}: "
+#   quantity_of_product = gets.to_i
+# end
+
+# quantity_of_product.times do |product|
+#   code = nil
+
+#   loop do
+#     print "Digite o código do produto #{product + 1}: "
+#     code = gets.to_i
+
+#     if code.zero?
+#       puts 'O código não pode estar vazio. Por favor, insira um código válido.'
+#     elsif products.any? { |p| p[:code] == code }
+#       puts 'Esse código já foi inserido. Por favor, insira um código diferente.'
+#     else
+#       break
+#     end
+#   end
+
+#   quantity = nil
+
+#   loop do
+#     print "Digite a quantidade do produto #{product + 1}: "
+#     quantity = gets.to_i
+
+#     if quantity.zero?
+#       puts 'A quantidade não pode estar vazia. Por favor, insira uma quantidade válida.'
+#     elsif quantity.negative?
+#       puts 'A quantidade deve ser um número positivo.'
+#     else
+#       break
+#     end
+#   end
+
+#   products << { code: code, quantity: quantity }
+# end
+
+# ## Consulta de produto
+
+# loop do
+#   print "\nDigite o código do produto (ou 0 para sair): "
+#   query_product = gets.chomp
+
+#   break if query_product == '0'
+
+#   if query_product.empty?
+#     puts 'O código não pode estar vazio. Por favor, insira um código válido.'
+#     next
+#   end
+
+#   query_product = query_product.to_i
+
+#   product = products.find { |p| p[:code] == query_product }
+
+#   if product
+#     quantity_found = product[:quantity]
+#     puts "Quantidade encontrada: #{quantity_found}"
+#   else
+#     puts 'Produto não encontrado'
+#   end
+# end
