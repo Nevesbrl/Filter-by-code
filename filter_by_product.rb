144a10b49d660c6318e0d760ccc21fb0740c2e1a
Thomas Neves <tomasdtna@gmal.com>
2023-06-16 23:18:59 -0300

    feat: add restriction for duplicate code

--- filter_by_product.rb
+++ filter_by_product.rb
@@ -7,8 +7,16 @@ print "\nNumber of products to register (max #{PRODUCT_MAX}): "
 quantityOfProduct = gets.chomp.to_i
 
 quantityOfProduct.times do |product|
-  print "Enter the product code #{product + 1}: "
-  code = gets.chomp.to_i
+  code = nil
+
+  loop do
+    print "Enter the product code #{product + 1}: "
+    code = gets.chomp.to_i
+
+    break unless products.any? { |product| product[:code] == code }
+
+    puts 'This code has already been entered. Please enter a different code.'
+  end
 
   print "Enter the quantity of the product #{product + 1}: "
   quantity = gets.chomp.to_i
